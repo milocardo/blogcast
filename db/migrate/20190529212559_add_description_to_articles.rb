cc24f2b0c7687886425c4d9e01426c0101a87341
Milene Cardoso <m.cardoso@hotmail.fr>
2019-06-02 13:47:19 -0300

    quick change on model user

--- db/migrate/20190529212559_add_description_to_articles.rb
+++ db/migrate/20190529212559_add_description_to_articles.rb
@@ -3,6 +3,5 @@ class AddDescriptionToArticles < ActiveRecord::Migration[5.1]
     add_column :articles, :description, :text
     add_column :articles, :created_at, :datetime
     add_column :articles, :updated_at, :datetimearticle = Article.find(2)
-
   end
 end
---
+++ db/migrate/20190602160429_add_timestamp_to_articles.rb
@@ -0,0 +1,9 @@
+class AddTimestampToArticles < ActiveRecord::Migration[5.1]
+def change
+  add_timestamps :articles, null: true
+  long_ago = DateTime.new(2019, 6, 2)
+  Article.update_all(created_at: long_ago, updated_at: long_ago)
+  change_column_null :articles, :created_at, false
+  change_column_null :articles, :updated_at, false
+end
+end
